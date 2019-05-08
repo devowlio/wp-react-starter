75ba11be1b86a3b161fb99c0c96953cd69fb40fb
Matthias Günter <matzeeable@gmail.com>
2019-05-08 18:29:34 +0200

    First experiments within VSCode

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,14 @@
 module.exports = function(grunt) {
     // Project configuration
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'), // DO NOT REMOVE
-        SERVE_DIR: 'dist', // The serve folder
+        pkg: grunt.file.readJSON("package.json"), // DO NOT REMOVE
+        SERVE_DIR: "dist", // The serve folder
         SERVE_POST_TASKS: [], // This grunt tasks runs after the serve is complete
         clean: {
             /**
              * DO NOT REMOVE. Task to clean the already copied node modules to the public library folder
              */
-            npmLibs: [
-                'public/lib/react/',
-                'public/lib/react-dom/',
-                'public/lib/mobx/',
-                'public/lib/mobx-state-tree/',
-            ] // Your library folders, do not use 'public/lib' as source directly
+            npmLibs: ["public/lib/react/", "public/lib/react-dom/", "public/lib/mobx/", "public/lib/mobx-state-tree/"] // Your library folders, do not use 'public/lib' as source directly
         },
         copy: {
             /**
@@ -23,32 +18,32 @@ module.exports = function(grunt) {
              */
             npmLibs: {
                 expand: true,
-                cwd: 'node_modules',
+                cwd: "node_modules",
                 src: [
-                    'react/umd/*.js',
-                    'react/LICENSE*',
-                    'react-dom/umd/react-dom.development.js',
-                    'react-dom/umd/react-dom.production.min.js',
-                    'react-dom/LICENSE*',
-                    'mobx/LICENSE*',
-                    'mobx/lib/mobx.umd*.js',
-                    'mobx/lib/mobx.umd*.map',
-                    'mobx-state-tree/dist/mobx-state-tree.umd.js'
+                    "react/umd/*.js",
+                    "react/LICENSE*",
+                    "react-dom/umd/react-dom.development.js",
+                    "react-dom/umd/react-dom.production.min.js",
+                    "react-dom/LICENSE*",
+                    "mobx/LICENSE*",
+                    "mobx/lib/mobx.umd*.js",
+                    "mobx/lib/mobx.umd*.map",
+                    "mobx-state-tree/dist/mobx-state-tree.umd.js"
                 ], // Your library files
-                dest: 'public/lib/'
+                dest: "public/lib/"
             }
         }
     });
-    
+
     // Load WP ReactJS Starter tasks (DO NOTE REMOVE this and initConfig should be called until here already)
-    require('./build/grunt.js')(grunt);
-    
+    require("./build/grunt.js")(grunt);
+
     // Load npm tasks
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+
     // Register default task
-    grunt.registerTask('default', function() {
+    grunt.registerTask("default", function() {
         grunt.log.write("Your default task...");
     });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,85 @@
-var path = require( 'path' ),
-	webpack = require( 'webpack' ),
-	exec = require('child_process').exec,
-	NODE_ENV = process.env.NODE_ENV || 'development',
-	MiniCssExtractPlugin = require( 'mini-css-extract-plugin' ),
-	dist = path.join( __dirname, 'public', NODE_ENV === 'production' ? 'dist' : 'dev' );
+var path = require("path"),
+    webpack = require("webpack"),
+    exec = require("child_process").exec,
+    NODE_ENV = process.env.NODE_ENV || "development",
+    MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+    dist = path.join(__dirname, "public", NODE_ENV === "production" ? "dist" : "dev");
 
 module.exports = {
-	mode: NODE_ENV,
-	entry: {
-		widget: './public/src/widget.js',
-		admin: './public/src/admin.js'
-	},
-	output: {
-		path: dist,
-		filename: "[name].js"
-	},
-	externals: {
-		'react': 'React',
-		'react-dom': 'ReactDOM',
-		'jquery': 'jQuery',
-		'mobx': 'mobx',
-		'mobx-state-tree': 'mobxStateTree',
-		'wp': 'wp',
-		'_': '_',
-		'wpApiSettings': 'wpApiSettings'
-	},
-	devtool: '#source-map',
-	module: {
-	    rules: [{
-	    	test: /\.js$/,
-			exclude: /(disposables)/,
-			use: 'babel-loader?cacheDirectory'
-	    }, {
-	    	test: /\.scss$/,
-	        use: [MiniCssExtractPlugin.loader, 'css-loader', {
-				loader: 'postcss-loader',
-				options: {
-					config: {
-						ctx: {
-							clean: {}
-						}
-					}
-				}
-			}, 'sass-loader']
-	    }]
-	},
-	resolve: {
-		extensions: [ '.js', '.jsx' ],
-		modules: [ 'node_modules', 'public/src' ]
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			// NODE_ENV is used inside React to enable/disable features that should only be used in development
-			'process.env': {
-				NODE_ENV: JSON.stringify( NODE_ENV ),
-				env: JSON.stringify( NODE_ENV )
-			}
-		}),
-		new MiniCssExtractPlugin( '[name].css' ),
-		new ((function() {
-			// Short plugin to run script on build finished to recreate the cachebuster
-			function WebPackRecreateCachebuster() { }
-			WebPackRecreateCachebuster.prototype.apply = function(compiler) {
-				compiler.plugin('done', function(compilation, callback) {
-					setTimeout(function() { console.log('Running webpack-build-done script...'); }, 0);
-					exec('npm run webpack-build-done', function(error, stdout, stderr) { console.log(stdout); });
-				});
-			};
-			return WebPackRecreateCachebuster;
-		})())()
-	]
+    mode: NODE_ENV,
+    entry: {
+        //widget: "./public/src/widget.js",
+        admin: "./public/src/admin.tsx"
+    },
+    output: {
+        path: dist,
+        filename: "[name].js"
+    },
+    externals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+        jquery: "jQuery",
+        mobx: "mobx",
+        "mobx-state-tree": "mobxStateTree",
+        wp: "wp",
+        _: "_",
+        wpApiSettings: "wpApiSettings"
+    },
+    devtool: "#source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                exclude: /(disposables)/,
+                use: "babel-loader?cacheDirectory"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            config: {
+                                ctx: {
+                                    clean: {}
+                                }
+                            }
+                        }
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        modules: ["node_modules", "public/src"]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            // NODE_ENV is used inside React to enable/disable features that should only be used in development
+            "process.env": {
+                NODE_ENV: JSON.stringify(NODE_ENV),
+                env: JSON.stringify(NODE_ENV)
+            }
+        }),
+        new MiniCssExtractPlugin("[name].css"),
+        new ((function() {
+            // Short plugin to run script on build finished to recreate the cachebuster
+            function WebPackRecreateCachebuster() {}
+            WebPackRecreateCachebuster.prototype.apply = function(compiler) {
+                compiler.plugin("done", function(compilation, callback) {
+                    setTimeout(function() {
+                        console.log("Running webpack-build-done script...");
+                    }, 0);
+                    exec("npm run webpack-build-done", function(error, stdout, stderr) {
+                        console.log(stdout);
+                    });
+                });
+            };
+            return WebPackRecreateCachebuster;
+        })())()
+    ]
 };
