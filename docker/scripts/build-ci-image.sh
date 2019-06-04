6205d9d7cf350f773ad5b3b63087183a084a38d8
Matthias Günter <matzeeable@gmail.com>
2019-06-04 22:05:25 +0200

    ci(test): built docker container for e2e

--- docker/scripts/build-ci-image.sh
+++ docker/scripts/build-ci-image.sh
@@ -7,3 +7,4 @@ fi
 CONTAINER_NAME=$(node -e "console.log(require('./package.json')['docker-ci-image-name'] || '')")
 
 echo "docker build --cache-from \"$CONTAINER_NAME:latest\" -t \"$CONTAINER_NAME:$VERSION\" \"$@\" ./docker/container/ci"
+docker build --cache-from "$CONTAINER_NAME:latest" -t "$CONTAINER_NAME:$VERSION" "$@" ./docker/container/ci
