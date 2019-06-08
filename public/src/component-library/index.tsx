3a3019638b712936f82dfd24bc88a16d14227077
Matthias Günter <matzeeable@gmail.com>
2019-06-08 13:20:37 +0200

    feat(ci): test CI pipeline

--- public/src/component-library/index.tsx
+++ public/src/component-library/index.tsx
@@ -22,7 +22,7 @@ async function doTestAjaxCall(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
 const ComponentLibrary: React.FunctionComponent<{}> = () => {
     return (
         <div className="wp-styleguide">
-            <h1>WP React Component Library</h1>
+            <h1>WP React Component Library Overview</h1>
 
             <Notice type={ENoticeType.Info}>
                 {translate("textDomainNotice", { args: { textDomain: pluginOptions.textDomain } })}
